3ee8fddb07cbc85a63e4501ddad0c9393b44100d
leander Oudakker <leander@oudcomb.com>
2021-01-04 15:45:09 +0100

    BI22PPWPP3-159, BI22PPWPP3-160 Added Generic Repository and BaseEntity

---
+++ Core.Domain/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Domain
+{
+    public abstract class BaseEntity : IEntity
+    {
+        [Key] public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public DateTime CreatedBy { get; set; }
+        public DateTime UpdatedBy { get; set; }
+        public DateTime DeletedBy { get; set; }
+    }
+}
---
+++ Core.Domain/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Domain
+{
+    public interface IEntity
+    {
+        [Key] public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public DateTime CreatedBy { get; set; }
+        public DateTime UpdatedBy { get; set; }
+        public DateTime DeletedBy { get; set; }
+    }
+}
