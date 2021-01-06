5e117d5e64eb24ee2b54cc1459e7aa7bc88b61e2
Leander Oudakker <leanderoudakker@users.noreply.github.com>
2021-01-06 15:09:51 +0100

    Update Intolerance.cs

--- Core.Domain/Intolerance.cs
+++ Core.Domain/Intolerance.cs
@@ -3,6 +3,8 @@
     public class Intolerance : BaseEntity
     {
         public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         
         public int ConsultationId { get; set; }
         public Consultation Consultation { get; set; }
