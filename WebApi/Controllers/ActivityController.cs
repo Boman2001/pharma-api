d5301e7054ec26237800cf855ba05629c65b971d
maartendonkersloot <51861725+maartendonkersloot@users.noreply.github.com>
2021-01-18 11:17:13 +0100

    Create ActivityController.cs

---
+++ WebApi/Controllers/ActivityController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Core.Domain.Models;
+using Core.DomainServices.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    public class ActivityController : Controller
+    {
+        private readonly IIdentityRepository _identityRepository;
+        private readonly IRepository<Activity> _activityRepository;
+
+        public ActivityController(
+            IRepository<Activity> activityRepository,
+            IIdentityRepository identityRepository)
+        {
+            _activityRepository = activityRepository;
+            _identityRepository = identityRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public ActionResult<IEnumerable<Activity>> Get()
+        {
+            return Ok(_activityRepository.Get());
+        }
+    }
+}
