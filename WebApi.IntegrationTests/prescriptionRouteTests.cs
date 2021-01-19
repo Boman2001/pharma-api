ba586ae7085d08e46d44f19a60bb0bfc9cfab0a1
maartendonkersloot <51861725+maartendonkersloot@users.noreply.github.com>
2021-01-19 14:11:32 +0100

    Fixed AdditionalExaminationTypeController and added prescription integration testss

---
+++ WebApi.IntegrationTests/prescriptionRouteTests.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Domain.Enums;
+using Core.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebApi.Models.Consultations;
+using WebApi.Models.Prescriptions;
+using Xunit;
+using Xunit.Extensions.Ordering;
+
+namespace WebApi.IntegrationTests
+{
+    public class prescriptionRouteTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private HttpClient _client;
+        private JToken _token;
+        private CustomWebApplicationFactory<Startup> _factory;
+
+        public prescriptionRouteTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact, Order(0)]
+        public async Task Get_Prescriptions_With_Token()
+        {
+            var tokenEnvironmentVariable = Environment.GetEnvironmentVariable("Token");
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenEnvironmentVariable);
+
+            var defaultPage = await _client.GetAsync("/api/prescriptions");
+            var content = defaultPage.Content.ReadAsStringAsync();
+
+            var json = content.Result;
+            var jArray = JArray.Parse(json);
+            var usersList = jArray.ToObject<List<Prescription>>();
+
+            Assert.Equal(HttpStatusCode.OK, defaultPage.StatusCode);
+            Assert.InRange(usersList.Count, 1, 100);
+        }
+
+        [Fact, Order(1)]
+        public async Task Get_Prescriptions_By_Id_With_Token()
+        {
+            var defaultPage = await _client.GetAsync("/api/prescriptions/" + 1);
+            var asStringAsync = defaultPage.Content.ReadAsStringAsync();
+            var json = asStringAsync.Result;
+            var jToken = JObject.Parse(json);
+            var user = jToken.ToObject<Prescription>();
+
+
+            Environment.SetEnvironmentVariable("prescriptions", json);
+
+            Assert.Equal(HttpStatusCode.OK, defaultPage.StatusCode);
+            Assert.NotNull(user);
+            Assert.IsType<Prescription>(user);
+        }
+
+        [Fact, Order(2)]
+        public async Task Get_Non_Existing_Prescriptions()
+        {
+            var tokenEnvironmentVariable = Environment.GetEnvironmentVariable("Token");
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenEnvironmentVariable);
+            var defaultPage = await _client.GetAsync("/api/prescriptions/1323232");
+
+            Assert.Equal(HttpStatusCode.NotFound, defaultPage.StatusCode);
+        }
+
+        [Fact, Order(3)]
+        public void Given_Valid_Post_Data_Posts()
+        {
+            var tokenEnvironmentVariable = Environment.GetEnvironmentVariable("Token");
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenEnvironmentVariable);
+
+            var admin = new IdentityUser
+            {
+                Id = "6105002a-295f-49b1-ace3-2072c7edbb69",
+                UserName = "m@gmail.com",
+                PhoneNumber = "+31623183611",
+                PhoneNumberConfirmed = true,
+                NormalizedUserName = "M@GMAIL.COM",
+                Email = "m@gmail.com",
+                NormalizedEmail = "M@GMAIL.COM",
+                EmailConfirmed = true,
+            };
+            Patient p = new Patient
+            {
+                Name = "jim",
+                Bsn = "bsn",
+                Email = "jim@jim.com",
+                Dob = DateTime.Now,
+                Gender = Gender.Other,
+                PhoneNumber = "124124",
+                City = "hank",
+                Street = "lepelaarstraat",
+                HouseNumber = "20",
+                HouseNumberAddon = "",
+                PostalCode = "4273cv",
+                Country = "Netherlands"
+            };
+
+
+            var resultPatient = _client.GetAsync("/api/patients/").Result;
+            var readAsStringAsyncPatient = resultPatient.Content.ReadAsStringAsync();
+            var jsonPatient = readAsStringAsyncPatient.Result;
+
+            var jArray = JArray.Parse(jsonPatient);
+            var patientList = jArray.ToObject<List<Patient>>();
+
+
+            var constulMessage = _client.GetAsync("/api/consultations/").Result;
+            var consuAsStringAsync = constulMessage.Content.ReadAsStringAsync();
+            var jsonResult = consuAsStringAsync.Result;
+
+            var jArrayr = JArray.Parse(jsonResult);
+            var consulList = jArrayr.ToObject<List<Consultation>>();
+
+            Prescription c = new Prescription
+            {
+                Id = 1,
+                Description = "descriptie",
+                Patient = patientList[0],
+                PatientId = patientList[0].Id,
+                ConsultationId = consulList[0].Id,
+                Consultation = consulList[0]
+            };
+
+            var serialize = JsonConvert.SerializeObject(c);
+
+            var content = new StringContent(serialize, Encoding.UTF8, "application/json");
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var result = _client.PostAsync("/api/prescriptions/", content).Result;
+            var readAsStringAsync = result.Content.ReadAsStringAsync();
+            var json = readAsStringAsync.Result;
+            var u = JObject.Parse(json);
+            var user = u.ToObject<Prescription>();
+            var userSerializeObject = JsonConvert.SerializeObject(user);
+
+            Assert.Equal(HttpStatusCode.Created, result.StatusCode);
+        }
+
+
+        [Fact, Order(3)]
+        public void Given_Invalid_Post_Data_Gives_Error()
+        {
+            var tokenEnvironmentVariable = Environment.GetEnvironmentVariable("Token");
+
+            _client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", tokenEnvironmentVariable);
+
+            Consultation c = new Consultation
+            {
+                Id = 1,
+                Date = DateTime.Now,
+            };
+
+            var serialize = JsonConvert.SerializeObject(c);
+
+            var content = new StringContent(serialize, Encoding.UTF8, "application/json");
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var result = _client.PostAsync("/api/prescriptions/", content).Result;
+            var readAsStringAsync = result.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+            Assert.InRange(readAsStringAsync.Result.Length, 10, int.MaxValue);
+        }
+
+        [Fact, Order(4)]
+        public async Task Update_prescriptions_And_test_If_User_Changed()
+        {
+            var environmentVariable = Environment.GetEnvironmentVariable("prescriptions");
+            var jObject = JObject.Parse(environmentVariable);
+            var dto = jObject.ToObject<Prescription>();
+
+            UpdatePrescriptionDto update = new UpdatePrescriptionDto
+            {
+                Description = "commentscomments",
+                PatientId = dto.Patient.Id,
+                ConsultationId = dto.Consultation.Id,
+                EndDate = DateTime.Now,
+                StartDate = DateTime.Now
+            };
+
+            var serialize = JsonConvert.SerializeObject(update);
+            var content = new StringContent(serialize, Encoding.UTF8, "application/json");
+
+            var defaultPage = await _client.PutAsync("/api/prescriptions/" + dto.Id, content);
+            var readAsStringAsync = defaultPage.Content.ReadAsStringAsync();
+            var json = readAsStringAsync.Result;
+            var u = JObject.Parse(json);
+            var user = u.ToObject<Prescription>();
+
+
+            var defaultPager = await _client.GetAsync("/api/prescriptions/" + dto.Id);
+            var asStringAsync = defaultPager.Content.ReadAsStringAsync();
+            var result = asStringAsync.Result;
+            var parsedJObject = JObject.Parse(result);
+            var userDto = parsedJObject.ToObject<Prescription>();
+
+            Assert.Equal(HttpStatusCode.OK, defaultPager.StatusCode);
+            Assert.NotNull(environmentVariable);
+            Assert.IsType<Prescription>(userDto);
+            Assert.Equal(update.Description, userDto.Description);
+            Assert.Equal(HttpStatusCode.OK, defaultPage.StatusCode);
+            Assert.NotNull(user);
+        }
+
+        [Fact, Order(4)]
+        public async Task Given_Invalid_Data_Update_Return_Bad_Request()
+        {
+            var environmentVariable = Environment.GetEnvironmentVariable("prescriptions");
+            var jObject = JObject.Parse(environmentVariable);
+            var userDto = jObject.ToObject<Consultation>();
+
+            var content = new StringContent("test", Encoding.UTF8, "application/json");
+
+            var defaultPage = await _client.PutAsync("/api/prescriptions/" + userDto.Id.ToString(), content);
+            var readAsStringAsync = defaultPage.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.BadRequest, defaultPage.StatusCode);
+            Assert.InRange(readAsStringAsync.Result.Length, 10, int.MaxValue);
+        }
+
+        [Fact, Order(6)]
+        public async Task Given_Prescription_Id_Deletes_prescriptions()
+        {
+            var userr = Environment.GetEnvironmentVariable("prescriptions");
+            var du = JObject.Parse(userr);
+            var udser = du.ToObject<Prescription>();
+
+            var tokenEnvironmentVariable = Environment.GetEnvironmentVariable("Token");
+
+            _client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", tokenEnvironmentVariable);
+            var defaultPage = await _client.DeleteAsync("/api/consultations/" + udser.Id.ToString());
+
+            Assert.Equal(HttpStatusCode.NoContent, defaultPage.StatusCode);
+        }
+    }
+}
---
+++ WebApi.Tests/ControllerTests/ConstulationsControllerTests.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core.Domain.Enums;
+using Core.Domain.Models;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebApi.controllers;
+using WebApi.Mappings;
+using WebApi.Models.Prescriptions;
+using WebApi.Tests.Helpers;
+using WebApi.Tests.Mocks;
+using WebApi.Tests.Mocks.Extends;
+using Xunit;
+
+namespace WebApi.Tests.ControllerTests
+{
+    public class ConstulationsControllerTests
+    {
+        private List<Prescription> _fakeEntities;
+        private List<Consultation> _constulatations;
+        private List<Patient> _patients;
+        private List<IdentityUser> _fakeIdentityUsers;
+        private PrescriptionsController FakeController { get; }
+        private IdentityRepository IdentityRepositoryFake { get; }
+        private List<UserInformation> _fakeUsersInformation;
+
+        public ConstulationsControllerTests()
+        {
+            SeedData();
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var mockMapper = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile()));
+            var mapper = mockMapper.CreateMapper();
+
+            var userManager = MockUserManager.GetMockUserManager(_fakeIdentityUsers).Object;
+            var signInManager = MockSigninManager.GetSignInManager<IdentityUser>(userManager).Object;
+
+            IdentityRepositoryFake = new IdentityRepository(userManager, signInManager, config);
+            var fakeGenericRepo = MockGenericRepository.GetUserInformationMock(_fakeEntities);
+
+            var fakeGenericRepoUserInformationMock = MockGenericRepository.GetUserInformationMock(_fakeUsersInformation);
+            MockUserExtension.ExtendMock(fakeGenericRepoUserInformationMock, _fakeUsersInformation);
+
+            var userInformationMock = MockGenericRepository.GetUserInformationMock(_patients);
+            var constulatationsMock = MockGenericRepository.GetUserInformationMock(_constulatations);
+            MockGenericExtension.ExtendMock(fakeGenericRepo, _fakeEntities);
+            FakeController = new PrescriptionsController(IdentityRepositoryFake, fakeGenericRepo.Object, fakeGenericRepoUserInformationMock.Object, userInformationMock.Object,
+                constulatationsMock.Object,
+                mapper);
+
+            IdentityHelper.SetUser(_fakeIdentityUsers[0], FakeController);
+        }
+
+        private void SeedData()
+        {
+            _fakeIdentityUsers = IdentityHelper.GetIdentityUsers();
+            var userInformation = new UserInformation
+            {
+                Name = "name",
+                City = "hank",
+                Street = "lepelaarstraat20",
+                HouseNumber = "20",
+                PostalCode = "23",
+                Country = "qwe",
+                UserId = Guid.Parse(_fakeIdentityUsers[0].Id)
+            };
+            _fakeUsersInformation = new List<UserInformation>();
+            _fakeUsersInformation.AddRange(new List<UserInformation>
+            {
+                userInformation
+            });
+
+            var p = new Patient
+            {
+                Name = "jim",
+                Bsn = "bsn",
+                Email = "jim@jim.com",
+                Dob = DateTime.Now,
+                Gender = Gender.Male,
+                PhoneNumber = "124124",
+                City = "hank",
+                Street = "lepelaarstraat",
+                HouseNumber = "20",
+                HouseNumberAddon = "",
+                PostalCode = "4273cv",
+                Country = "Netherlands"
+            };
+
+            var type = new AdditionalExaminationType
+            {
+                Name = "typename",
+                Unit = "GPS"
+            };
+            var additional = new AdditionalExaminationResult
+            {
+                Value = "value",
+                Date = DateTime.Now,
+                AdditionalExaminationType = type
+            };
+            var ipCode = new IcpcCode
+            {
+                Name = "Name",
+                Code = "code"
+            };
+            var ep = new Episode
+            {
+                Description = "Description",
+                Priority = 10,
+                Patient = p,
+                IcpcCode = ipCode
+            };
+            var intolerances = new Intolerance
+            {
+                Description = "descrption",
+                EndDate = DateTime.Now,
+                StartDate = DateTime.Now,
+                Patient = p
+            };
+            var physical = new PhysicalExamination()
+            {
+                Value = "physical",
+                Date = DateTime.Now,
+                Patient = p
+            };
+            var c = new Consultation
+            {
+                Id = 1,
+                Date = DateTime.Now,
+                Comments = "comments",
+                DoctorId = Guid.Parse(_fakeIdentityUsers[0].Id),
+                Doctor = _fakeIdentityUsers[0],
+                Patient = p,
+                AdditionalExaminationResults = new List<AdditionalExaminationResult>
+                {
+                    additional
+                },
+                Episodes = new List<Episode>
+                {
+                    ep
+                },
+                Intolerances = new List<Intolerance>
+                {
+                    intolerances
+                },
+                PhysicalExaminations = new List<PhysicalExamination>
+                {
+                    physical
+                }
+            };
+
+            var activity = new Prescription
+            {
+                Id = 1,
+                Description = "description",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.MaxValue,
+                Patient = p,
+                Consultation = c
+            };
+            var activity02 = new Prescription
+            {
+                Id = 2,
+                Description = "description",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.MaxValue,
+                Patient = p,
+                Consultation = c
+            };
+            _fakeEntities = new List<Prescription>
+            {
+                activity, activity02
+            };
+
+            _constulatations = new List<Consultation>
+            {
+                c
+            };
+            _patients = new List<Patient>
+            {
+                p
+            };
+        }
+    }
+}
