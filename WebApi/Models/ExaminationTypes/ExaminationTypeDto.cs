4d015b3cd760786f276189efb1ee946f9137ecb8
leander Oudakker <leander@oudcomb.com>
2021-01-20 15:05:20 +0100

    Updated ExaminationTypeController.cs

---
+++ WebApi/Models/ExaminationTypes/ExaminationTypeDto.cs
@@ -0,0 +1,9 @@
+﻿using WebApi.Models.AdditionalExaminationTypes;
+
+namespace WebApi.Models.ExaminationTypes
+{
+    public class ExaminationTypeDto : BaseAdditionalExaminationTypeDto
+    {
+        public int Id { get; set; }
+    }
+}
