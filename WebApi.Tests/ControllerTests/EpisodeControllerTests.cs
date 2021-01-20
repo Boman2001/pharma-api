dd5dd6cf99136b1e94ffac643f27f76cd59bbb0b
leander Oudakker <leander@oudcomb.com>
2021-01-20 16:09:43 +0100

    Fixed tests

--- WebApi.Tests/ControllerTests/EpisodeControllerTests.cs
+++ WebApi.Tests/ControllerTests/EpisodeControllerTests.cs
@@ -52,7 +52,7 @@ namespace WebApi.Tests.ControllerTests
 
             MockGenericExtension.ExtendMock(fakeGenericRepo, _fakeEntities);
             FakeController = new EpisodesController(
-                IdentityRepositoryFake, 
+                IdentityRepositoryFake,
                 fakeGenericRepo.Object,
                 constulatationsMock.Object,
                 userInformationMock.Object,
@@ -66,7 +66,7 @@ namespace WebApi.Tests.ControllerTests
         [Fact]
         public void Get_All_Episode_With_200_code()
         {
-            var result = FakeController.Get(null, null);
+            var result = FakeController.Get(null, null, false);
             var objectResult = (OkObjectResult) result.Result;
             var activities = (List<EpisodeDto>) objectResult.Value;
 
@@ -142,13 +142,11 @@ namespace WebApi.Tests.ControllerTests
             _fakeIdentityUsers = IdentityHelper.GetIdentityUsers();
             var activity = new Episode
             {
-                Id = 1,
-                Description = "description"
+                Id = 1, Description = "description"
             };
             var activity02 = new Episode
             {
-                Id = 2,
-                Description = "description"
+                Id = 2, Description = "description"
             };
             _fakeEntities = new List<Episode>
             {
@@ -173,39 +171,27 @@ namespace WebApi.Tests.ControllerTests
 
             var type = new AdditionalExaminationType
             {
-                Name = "typename",
-                Unit = "GPS"
+                Name = "typename", Unit = "GPS"
             };
             var additional = new AdditionalExaminationResult
             {
-                Value = "value",
-                Date = DateTime.Now,
-                AdditionalExaminationType = type
+                Value = "value", Date = DateTime.Now, AdditionalExaminationType = type
             };
             var ipCode = new IcpcCode
             {
-                Name = "Name",
-                Code = "code"
+                Name = "Name", Code = "code"
             };
             var episode = new Episode
             {
-                Description = "Description",
-                Priority = 10,
-                Patient = patient,
-                IcpcCode = ipCode
+                Description = "Description", Priority = 10, Patient = patient, IcpcCode = ipCode
             };
             var intolerance = new Intolerance
             {
-                Description = "descrption",
-                EndDate = DateTime.Now,
-                StartDate = DateTime.Now,
-                Patient = patient
+                Description = "descrption", EndDate = DateTime.Now, StartDate = DateTime.Now, Patient = patient
             };
             var physical = new PhysicalExamination()
             {
-                Value = "physical",
-                Date = DateTime.Now,
-                Patient = patient
+                Value = "physical", Date = DateTime.Now, Patient = patient
             };
             var consultation = new Consultation
             {
@@ -244,8 +230,7 @@ namespace WebApi.Tests.ControllerTests
             };
             var code = new IcpcCode()
             {
-                Code = "code",
-                Name = "name"
+                Code = "code", Name = "name"
             };
 
             _icpcCodes = new List<IcpcCode>
